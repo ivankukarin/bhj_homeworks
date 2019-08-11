4fe5ba95f4a6c8a063a65e2c92caa3cff68168a0
ivankukarin <ikukarin@yandex.ru>
2019-08-11 17:53:35 +0400

    task 1.3 is done

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,19 +1,36 @@
-const f= () => {
+const catchMole = () => {
   let dead = document.getElementById('dead');
   let lost = document.getElementById('lost');
+  const
+  const startPosition = () => {
+    dead.textContent = 0;
+    lost.textContent = 0;
+  }
+
 
   const getHole = (index) => document.getElementById(`hole${index}`);
   
   for (let i = 1; i <= 9; i++){
     let el = getHole(i);
-    const checkMole = () => {if (el.classList.contains('hole hole_has-mole')){
+    const checkMole = () => {if (el.classList.contains('hole_has-mole')){
       dead.textContent++
       } else {
       lost.textContent++
       };
+      if (dead.textContent === '10'){
+        alert('Победа!');
+        startPosition();
+      }
+      if (lost.textContent === '5'){
+        alert('Вы проиграли!');
+        startPosition();
+      }
     };
     el.addEventListener('click', checkMole)
+    
   }
+  
+
 };
 
-f();
+catchMole();
