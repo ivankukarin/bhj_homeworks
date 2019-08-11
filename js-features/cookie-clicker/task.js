6582339f301b232c23db0abb982d3e5fd9fc69bb
ivankukarin <ikukarin@yandex.ru>
2019-08-11 11:09:46 +0400

    added 'speed of click'

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,10 +1,20 @@
 let image = document.getElementById('cookie');
-let countClick = document.getElementById('clicker__counter')
+let countClick = document.getElementById('clicker__counter');
+let speedClick = document.getElementById('speed__click');
+let start;
+let end;
+let timeClickSec;
 
 function increaseSizeImg (){
+  end = new Date;
+  timeClickSec = (1000/((end - start))).toFixed(2);
+  speedClick.textContent = timeClickSec;
+
   image.width = 250;
   countClick.textContent++;
   setTimeout(()=>image.width = 200, 1)
+
+  start = new Date;
 }
 
 image.addEventListener('click', increaseSizeImg );
