a21eff91626f9a4a2e3ef08bebd705d765f2d724
ivankukarin <ikukarin@yandex.ru>
2019-08-13 12:53:05 +0400

    task MENU with high level difficulty  is done

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,15 +1,19 @@
 
 let menuLinkAll = document.querySelectorAll('.menu__link');
-let menuSub = document.getElementsByClassName('menu_active');
+
 
 
 for (let i = 0; i < menuLinkAll.length; i++ ){
   let menuLinkWithSub = menuLinkAll[i].closest('.menu__item').querySelector('ul.menu_sub');
   if(menuLinkWithSub){ menuLinkAll[i].onclick =  () =>{
-    console.log(menuLinkAll);
+    let menuSubActive = document.querySelector('.menu_active');
+    
     if (menuLinkWithSub.classList.contains('menu_active')) {
       menuLinkWithSub.classList.remove('menu_active')
-      } else {
+      } else if (menuSubActive){
+        menuSubActive.classList.remove('menu_active');
+        menuLinkWithSub.classList.add('menu_active');
+        } else {
       menuLinkWithSub.classList.add('menu_active')
       };
       return false;
