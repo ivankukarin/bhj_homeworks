176b2a635cca5fb977fecffaa7499a01122cbec6
ivankukarin <ikukarin@yandex.ru>
2019-08-13 14:11:04 +0400

    changed 'onclick' - 'addEventListener'

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,7 +5,8 @@ let menuLinkAll = document.querySelectorAll('.menu__link');
 
 for (let i = 0; i < menuLinkAll.length; i++ ){
   let menuLinkWithSub = menuLinkAll[i].closest('.menu__item').querySelector('ul.menu_sub');
-  if(menuLinkWithSub){ menuLinkAll[i].onclick =  () =>{
+  if(menuLinkWithSub){ menuLinkAll[i].addEventListener('click', (event) =>{
+    event.preventDefault();
     let menuSubActive = document.querySelector('.menu_active');
     
     if (menuLinkWithSub.classList.contains('menu_active')) {
@@ -16,8 +17,8 @@ for (let i = 0; i < menuLinkAll.length; i++ ){
         } else {
       menuLinkWithSub.classList.add('menu_active')
       };
-      return false;
-    };
+      
+    })
   }
 
 }
