e9270adba58e9c905f67f505d8801f24a14d5acc
ivankukarin <ikukarin@yandex.ru>
2019-08-16 20:02:10 +0400

    deleted console.log's

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,13 +1,12 @@
 let revealElems = document.querySelectorAll('.reveal');
 
-console.log(revealElems);
+
 
 for (let el of revealElems){
   window.addEventListener('scroll', ()=>{
   let viewportHeight = window.innerHeight;
   let elemBotoom = el.getBoundingClientRect().bottom;
   let elemTop = el.getBoundingClientRect().top
-  console.log(viewportHeight, elemBotoom, elemTop)
   if ( elemBotoom <= viewportHeight && elemTop < viewportHeight && elemBotoom >= 0) {
     el.classList.add('reveal_active')
   } else {
