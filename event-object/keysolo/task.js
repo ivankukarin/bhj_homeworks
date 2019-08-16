650905f3510874c870081f1223966cd2f871f391
ivankukarin <ikukarin@yandex.ru>
2019-08-16 09:52:59 +0400

    task keysolo is done

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,22 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
-  }
+    
+    document.addEventListener('keydown', (evt)=>{
+      let inputChar;
+      inputChar = evt.key.toUpperCase();
+          
+      if (inputChar === this.currentSymbol.textContent.toUpperCase()) {
+        this.success(); 
+        } else {
+        this.fail();
+        }
+      }    
+      
+    )
+  } 
+    
+  
 
   success() {
     this.currentSymbol.classList.add('symbol_correct');
