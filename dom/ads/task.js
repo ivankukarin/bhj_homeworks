b3f13d638100b87b1e5afe1c8484e0ece99a47c4
ivankukarin <ikukarin@yandex.ru>
2019-08-18 14:25:35 +0400

    deleted console.log

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -9,14 +9,13 @@ function removeClass(element, nameClass) {
 
 
 
-function changePhrase (){
-  console.log(rotateCaseAll[i]);
-  for (let rotateCase of rotateCaseAll){
+function changePhrase () {
+  for (let rotateCase of rotateCaseAll) {
     removeClass(rotateCase, 'rotator__case_active');
   }
   rotateCaseAll[i].classList.add('rotator__case_active');
   i++
-  if (i=== rotateCaseAll.length) { i = 0};
+  if (i=== rotateCaseAll.length) { i = 0 };
   };
 
 setInterval(changePhrase, 1000);
