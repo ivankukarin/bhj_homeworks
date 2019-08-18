b011138fcc16bd46dc0cb2652b248645c2cd96b8
ivankukarin <ikukarin@yandex.ru>
2019-08-18 14:10:58 +0400

    task 'ads' is done

---
+++ dom/ads/task.js
@@ -0,0 +1,22 @@
+let rotateCaseAll = document.querySelectorAll('span.rotator__case');
+let i = 0;
+
+function removeClass(element, nameClass) {
+    if (element.classList.contains(nameClass)) {
+        element.classList.remove(nameClass)
+    }
+  };
+
+
+
+function changePhrase (){
+  console.log(rotateCaseAll[i]);
+  for (let rotateCase of rotateCaseAll){
+    removeClass(rotateCase, 'rotator__case_active');
+  }
+  rotateCaseAll[i].classList.add('rotator__case_active');
+  i++
+  if (i=== rotateCaseAll.length) { i = 0};
+  };
+
+setInterval(changePhrase, 1000);
